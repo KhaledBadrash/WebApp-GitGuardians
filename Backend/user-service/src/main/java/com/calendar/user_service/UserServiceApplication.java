c608c95657a88e0399d40575b4f2ee166ad271b1
zidanzmg <168085010+zidanzmg@users.noreply.github.com>
2025-02-03 21:18:39 +0100

    Ergänzung User Service Application

--- Backend/user-service/src/main/java/com/calendar/user_service/UserServiceApplication.java
+++ Backend/user-service/src/main/java/com/calendar/user_service/UserServiceApplication.java
@@ -3,8 +3,17 @@ package com.calendar.user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Hauptklasse der Benutzer-Service-Anwendung.
+ * Diese Klasse startet die Spring Boot Anwendung.
+ */
 @SpringBootApplication
 public class UserServiceApplication {
+    
+    /**
+     * Einstiegspunkt der Anwendung.
+     * Die Spring Boot Anwendung wird hier initialisiert und gestartet.
+     */
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
